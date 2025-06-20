ce53191536d2a440487ba3baa6ee76c4f46aadc6
Ubuntu <ubuntu@ip-172-31-94-210.ec2.internal>
2025-06-20 06:27:23 +0000

    fix: detail artikel

--- resources/views/artikel.blade.php
+++ resources/views/artikel.blade.php
@@ -16,34 +16,33 @@
     <section class="product-list-section overflow-hidden section-padding white-bg">
         <div class="container">
             <div class="row g-xl-4 g-3 justify-content-center">
-                <div class="col-lg-9">
-                    @foreach ($artikels as $artikel)
-                        <div class="col-lg-4 col-md-6 col-sm-11 wow fadeInUp" data-wow-delay=".{{ 3 + $loop->index * 2 }}s">
-                            <div class="blog-itemsv1">
-                                <div class="thumb w-100">
-                                    <img src="{{ asset('image_artikel/' . $artikel->image) }}" alt="{{ $artikel->judul }}"
-                                        class="w-100">
-                                    <div class="dates">{{ \Carbon\Carbon::parse($artikel->created_at)->format('d M') }}
-                                    </div>
-                                </div>
-                                <div class="content">
-                                    <ul class="comment-inner">
-                                        <li class="text-success "><i class="text-success fa-regular fa-user"></i>
-                                            {{ $artikel->user->nama ?? 'Admin' }}</li>
-                                        <span class="badge bg-success me-1"><i class="fa-regular fa-tags me-1"></i>
-                                            {{ $artikel->tags ?? '-' }}</span>
-                                    </ul>
-                                    <a href="{{ url('artikel-detail/' . $artikel->slug) }}"
-                                        class="title">{{ $artikel->judul }}</a>
-                                    <p>{{ \Illuminate\Support\Str::limit(strip_tags($artikel->isi), 90) }}</p>
-                                    <a href="{{ url('artikel-detail/' . $artikel->slug) }}" class="arrows">
-                                        Selengkapnya <i class="fa-solid fa-arrow-right"></i>
-                                    </a>
+                @foreach ($artikels as $artikel)
+                    <div class="col-lg-4 col-md-6 col-sm-11 wow fadeInUp" data-wow-delay=".{{ 3 + $loop->index * 2 }}s">
+                        <div class="blog-itemsv1">
+                            <div class="thumb w-100">
+                                <img src="{{ asset('image_artikel/' . $artikel->image) }}" alt="{{ $artikel->judul }}"
+                                    class="w-100">
+                                <div class="dates">{{ \Carbon\Carbon::parse($artikel->created_at)->format('d M') }}
                                 </div>
                             </div>
+                            <div class="content">
+                                <ul class="comment-inner">
+                                    <li class="text-success "><i class="text-success fa-regular fa-user"></i>
+                                        {{ $artikel->user->nama ?? 'Admin' }}</li>
+                                    <span class="badge bg-success me-1"><i class="fa-regular fa-tags me-1"></i>
+                                        {{ $artikel->tags ?? '-' }}</span>
+                                </ul>
+                                <a href="{{ url('artikel-detail/' . $artikel->slug) }}"
+                                    class="title">{{ $artikel->judul }}</a>
+                                <p>{{ \Illuminate\Support\Str::limit(strip_tags($artikel->isi), 90) }}</p>
+                                <a href="{{ url('artikel-detail/' . $artikel->slug) }}" class="arrows">
+                                    Detail Artikel <i class="fa-solid fa-arrow-right"></i>
+                                </a>
+                            </div>
                         </div>
-                    @endforeach
-                </div>
+                    </div>
+                @endforeach
+                
             </div>
 
             <div class="row">
