60694452825ec1510a84b313e52584883aec1b6a
Agas Triawan <agastriawan@gmail.com>
2025-06-20 09:05:14 +0700

    new: image testimonial

--- resources/views/sections/testimoni.blade.php
+++ resources/views/sections/testimoni.blade.php
@@ -15,7 +15,7 @@
             <div class="row g-6 align-items-center">
                 <div class="col-lg-4 col-md-4">
                     <div class="testimonial-thumbv1 w-100 wow fadeInDown" data-wow-delay=".4s">
-                        <img src="{{ asset('assets/img/testimonial/testimoni.jpg') }}" alt="img" class="w-100"
+                        <img src="{{ asset('assets/img/testimonial/testimonials.jpg') }}" alt="img" class="w-100"
                             style="width: 440px; height: 556px; object-fit: cover; border-radius: 8px;">
                     </div>
                 </div>
